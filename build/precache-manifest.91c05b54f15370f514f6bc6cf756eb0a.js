1112a52cbf4fe10103bfec030c39dc51228b579a
Ivan <vanchas@protonmail.com>
2020-04-18 01:16:32 +0300

    build

---
+++ build/precache-manifest.91c05b54f15370f514f6bc6cf756eb0a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45afd1e7ca4c3e6dbeb0b3dec483653e",
+    "url": "/Prozorro/index.html"
+  },
+  {
+    "revision": "f7b9af20cd402e05a8b4",
+    "url": "/Prozorro/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ccfc5cb089ed113059af",
+    "url": "/Prozorro/static/css/main.f25f55ab.chunk.css"
+  },
+  {
+    "revision": "f7b9af20cd402e05a8b4",
+    "url": "/Prozorro/static/js/2.4f8482db.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/Prozorro/static/js/2.4f8482db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccfc5cb089ed113059af",
+    "url": "/Prozorro/static/js/main.33d85cc1.chunk.js"
+  },
+  {
+    "revision": "b04d30fa2d3b47d72762",
+    "url": "/Prozorro/static/js/runtime-main.a206e397.js"
+  },
+  {
+    "revision": "31c1277e17488528fa2a4089ceaf7800",
+    "url": "/Prozorro/static/media/1.31c1277e.png"
+  },
+  {
+    "revision": "853e46b08ea3420eaacfda1a3c208c10",
+    "url": "/Prozorro/static/media/2-lil-rob.853e46b0.png"
+  },
+  {
+    "revision": "14646a6f165555c277afd494c806c476",
+    "url": "/Prozorro/static/media/2.14646a6f.png"
+  },
+  {
+    "revision": "d04e793931d4e2b68b3af6ad531c55e9",
+    "url": "/Prozorro/static/media/3.d04e7939.png"
+  },
+  {
+    "revision": "7d17e4e45b2789d961c8c738a9f27e50",
+    "url": "/Prozorro/static/media/4.7d17e4e4.png"
+  },
+  {
+    "revision": "e200085b02670a69234d4c791c75769b",
+    "url": "/Prozorro/static/media/5.e200085b.png"
+  },
+  {
+    "revision": "cfc289340b27b91ed52f3325a5692b15",
+    "url": "/Prozorro/static/media/6.cfc28934.png"
+  },
+  {
+    "revision": "2453947a58420dbb0b6c53c637dfa6c7",
+    "url": "/Prozorro/static/media/big-rob.2453947a.png"
+  },
+  {
+    "revision": "69a57721c9cbee88795faedbf8e5212b",
+    "url": "/Prozorro/static/media/header-logo.69a57721.png"
+  }
+]);
