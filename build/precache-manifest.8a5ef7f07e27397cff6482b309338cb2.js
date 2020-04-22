0662431ad7521a66aab8b2f9c4ef2be2acccd20b
Ivan <vanchas@protonmail.com>
2020-04-22 13:31:01 +0300

    result page

---
+++ build/precache-manifest.8a5ef7f07e27397cff6482b309338cb2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626049251a589dcddefd1a3b66b5b8ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07c912dbdb967ad3deb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "07c95def62ace41ef3e0",
+    "url": "/static/css/main.669c617e.chunk.css"
+  },
+  {
+    "revision": "b07c912dbdb967ad3deb",
+    "url": "/static/js/2.13de3c0b.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/static/js/2.13de3c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c95def62ace41ef3e0",
+    "url": "/static/js/main.254058a7.chunk.js"
+  },
+  {
+    "revision": "70c389307114c94b380e",
+    "url": "/static/js/runtime-main.bfce489e.js"
+  },
+  {
+    "revision": "31c1277e17488528fa2a4089ceaf7800",
+    "url": "/static/media/1.31c1277e.png"
+  },
+  {
+    "revision": "853e46b08ea3420eaacfda1a3c208c10",
+    "url": "/static/media/2-lil-rob.853e46b0.png"
+  },
+  {
+    "revision": "14646a6f165555c277afd494c806c476",
+    "url": "/static/media/2.14646a6f.png"
+  },
+  {
+    "revision": "d04e793931d4e2b68b3af6ad531c55e9",
+    "url": "/static/media/3.d04e7939.png"
+  },
+  {
+    "revision": "7d17e4e45b2789d961c8c738a9f27e50",
+    "url": "/static/media/4.7d17e4e4.png"
+  },
+  {
+    "revision": "e200085b02670a69234d4c791c75769b",
+    "url": "/static/media/5.e200085b.png"
+  },
+  {
+    "revision": "cfc289340b27b91ed52f3325a5692b15",
+    "url": "/static/media/6.cfc28934.png"
+  },
+  {
+    "revision": "2453947a58420dbb0b6c53c637dfa6c7",
+    "url": "/static/media/big-rob.2453947a.png"
+  },
+  {
+    "revision": "69a57721c9cbee88795faedbf8e5212b",
+    "url": "/static/media/header-logo.69a57721.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Prozorro/precache-manifest.96ff97bc49b97a4620242e23f9083782.js"
+  "/precache-manifest.8a5ef7f07e27397cff6482b309338cb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Prozorro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
