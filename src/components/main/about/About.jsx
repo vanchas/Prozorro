c560250b77d32fa93128b725e0a48dc64c540e76
Ivan <vanchas@protonmail.com>
2020-04-22 09:22:18 +0300

    remove comments

--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -62,7 +62,6 @@ export default class About extends Component {
 
         <header className="row m-0 p-0 about-header">
           <div className="px-lg-3 px-md-0 col-lg-8 col-md-12 header-description">
-            {/* <div className=""></div> */}
             <div className="info-header-block">
               <div className="bkg-info text-white text-center about-header-description-top pt-2">
                 <h1 className="font-weight-bold">Я ТЕНДЕРБОТ</h1>
@@ -70,7 +69,6 @@ export default class About extends Component {
               </div>
               <div className="about-header-description-top-inner font-weight-bolder pt-2 pb-3">
                 <div className="about-header-description-top-inner-item">
-                  <div className=""></div>
                   <div className="inner-content">
                     <div className="img-holder pr-1">
                     <img src={MolotSign} alt="." className="molot" />
@@ -79,7 +77,6 @@ export default class About extends Component {
                   </div>
                 </div>
                 <div className="about-header-description-top-inner-item">
-                  <div className=""></div>
                   <div className="inner-content">
                     <div className="img-holder pr-2">
                     <img className="pt-1" src={DocSign} alt="." />
@@ -88,7 +85,6 @@ export default class About extends Component {
                   </div>
                 </div>
                 <div className="about-header-description-top-inner-item">
-                  <div className=""></div>
                   <div className="inner-content">
                     <div className="img-holder pr-1">
                     <img src={ScalesSign} alt="." />
@@ -147,90 +143,6 @@ export default class About extends Component {
             </div>
           </div>
         </header>
-
-        {/* <header className="row m-0 p-0 about-header mx-auto">
-          <div className="bg-danger header-description row col-lg-8 col-md-12 mt-1 pl-0 pr-lg-3 pr-md-0 mx-md-auto">
-            <div className="col-lg-2 col-md-0 p-0 m-0"></div>
-            <div className="info-header-block col-lg-10 col-md-12 pr-lg-1 pr-md-0 pl-0">
-              <div className="bkg-info text-white text-center about-header-description-top">
-                <h1 className="m-0 h2 font-weight-bold">Я ТЕНДЕРБОТ</h1>
-                <p className="h6 pl-lg-5 pl-md-0 pb-2">УНИКАЛЬНАЯ КОМПЬЮТЕРНАЯ ПРОГРАММА</p>
-              </div>
-              <div className="about-header-description-top-inner font-weight-bolder pb-2">
-                <div className="row w-100">
-                  <div className="col-lg-3 col-md-0 p-0"></div>
-                  <div className="col-lg-9 col-md-12 pl-md-2 pl-sm-0 pl pr-0 d-flex">
-                    <img height={35} src={MolotSign} alt="." />
-                    <p className="pl-2 m-0 header-text-item">ОБРАБОТАЮ ТЕНДЕРНУЮ ДОКУМЕНТАЦИЮ</p>
-                  </div>
-                </div>
-                <div className="row w-100">
-                  <div className="col-lg-3 col-md-0 p-0"></div>
-                  <div className="col-lg-9 col-md-12 pl-md-2 pl-sm-0 d-flex pl-2">
-                    <img className="mt-2" height={28} src={DocSign} alt="." />
-                    <p className="pl3 mb-2 header-text-item">СОСТАВЛЮ СПИСОК НЕОБХОДИМЫХ ДОКУМЕНТОВ</p>
-                  </div>
-                </div>
-                <div className="row w-100">
-                  <div className="col-lg-3 col-md-0 p-0"></div>
-                  <div className="col-lg-9 col-md-12 pl-md-2 pl-sm-0 d-flex pl-2">
-                    <img height={30} src={ScalesSign} alt="." className="mt-1" />
-                    <p className="pl-3 m-0 header-text-item">ОТПРАВЛЮ ЕГО ВАМ ПОСЛЕ ПРОВЕРКИ ОПЫТНЫМ ЮРИСТОМ.</p>
-                  </div>
-                </div>
-              </div>
-              <div className="bkg-info text-white text-center">
-                <p className="h2 pt-3 pb-3 pl-lg-5 ml-lg-4 pl-md-0 ml-md-0 font-weight-bold">Я БЫСТРЕЕ ВСЕХ!</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="text-dark mt-1 tender-form col-lg-4 col-md-12 pt-2 pb-0">
-            <h2 className="text-center mb-2 h5 font-weight-bold">ПЛАНИРУЕТЕ УЧАСТВОВАТЬ В ТЕНДЕРЕ?</h2>
-            <p className="text-center font-weight-normal small px-5">Получите список необходимых документов для участия в тендере.</p>
-            <form className="form-group d-flex flex-column justify-items-center">
-              <input type="number"
-                onChange={e => this.phoneInput(e.target.value)}
-                className="p-1 rounded border border-light mb-1"
-                placeholder="Телефон" />
-              <input type="email"
-                onChange={e => this.emailInput(e.target.value)}
-                className="p-1 rounded border border-light mb-1"
-                placeholder="E-mail" />
-              <input type="text"
-                onChange={e => this.linkInput(e.target.value)}
-                className="p-1 rounded border border-light mb-2"
-                placeholder="Ссылка на тендер в прозорро" />
-              <div className=" d-flex justify-content-center">
-                <button className="btn text-white bkg-info px-4 py-1"
-                  onClick={e => this.sendData(e)}>
-                  Получить бесплатно</button>
-              </div>
-            </form>
-          </div>
-
-          <div className="col-lg-6 col-md-0"></div>
-
-          <div className="col-lg-6 col-md-12 row py-0 px-2 m-0">
-            <div className="big-num-item col-lg-4 col-md-4 col-sm-12 px-1 d-flex flex-column">
-              <span className="txt-info big-num font-weight-bold">10</span>
-              <span className="h6">За сегодня обработано тендеров</span>
-            </div>
-            <div className="big-num-item col-lg-4 col-md-4 col-sm-12 px-1 d-flex flex-column">
-              <span className="txt-info big-num font-weight-bold">500</span>
-              <span className="h6">За месяц обработано тендеров</span>
-            </div>
-            <div className="big-num-item col-lg-4 col-md-4 col-sm-12 px-1 d-flex flex-column block-wrap">
-              <span className="txt-info big-num font-weight-bold">
-                <span> 2,5</span>
-                <small className="h6 min">минуты</small>
-              </span>
-              <span className="h6">Лучший результат на тендер</span>
-            </div>
-          </div>
-        </header> */}
-
-
       </div>
     )
   }
